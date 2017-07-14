054be3d8fb4b07700b55a2a5429edf8809c789e0
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-07-14 20:33:19 -0300

    Ativando Onclicks das telas

--- app/src/main/java/com/ufrpe/safecampus/View/TelaPrincipalActivity.java
+++ app/src/main/java/com/ufrpe/safecampus/View/TelaPrincipalActivity.java
@@ -3,6 +3,8 @@ package com.ufrpe.safecampus.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ufrpe.safecampus.R;
+
 public class TelaPrincipalActivity extends AppCompatActivity {
 
     @Override
