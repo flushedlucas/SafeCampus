11d2b00cd84e03c6a90c8e005f8f148db2028af2
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-07-14 17:35:24 -0300

    Criação das Telas

---
+++ app/src/main/java/com/ufrpe/safecampus/BotaoPanicoActivity.java
@@ -0,0 +1,13 @@
+package com.ufrpe.safecampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BotaoPanicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_botao_panico);
+    }
+}
---
+++ app/src/main/java/com/ufrpe/safecampus/TipoOcorrenciaActivity.java
@@ -0,0 +1,13 @@
+package com.ufrpe.safecampus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TipoOcorrenciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipo_ocorrencia);
+    }
+}
