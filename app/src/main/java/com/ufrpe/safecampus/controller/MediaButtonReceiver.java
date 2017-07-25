c7ff5337e2f59e418b7d3aa4b9abb1f5dfebb21e
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-07-25 00:22:44 -0300

    Botão pânico ativado pelo switch

--- app/src/main/java/com/ufrpe/safecampus/controller/MediaButtonReceiver.java
+++ app/src/main/java/com/ufrpe/safecampus/controller/MediaButtonReceiver.java
@@ -16,20 +16,20 @@ public class MediaButtonReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Session session = Session.getInstanciaSessao();
         if (session.getPanico()) {
-//            int prevVolume;
-//            int volume;
-//            if ("android.media.VOLUME_CHANGED_ACTION".equals(intent.getAction())) {
-//                volume = (int) intent.getExtras().get("android.media.EXTRA_VOLUME_STREAM_VALUE");
-//                prevVolume = (int) intent.getExtras().get("android.media.EXTRA_PREV_VOLUME_STREAM_VALUE");
-//                if (volume < prevVolume) {
-//                    Toast.makeText(context, "Volume down!", Toast.LENGTH_SHORT).show();
-//
-//                } else if (volume > prevVolume) {
-//                    Toast.makeText(context, "Volume up!", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    Toast.makeText(context, "Volume equals!", Toast.LENGTH_SHORT).show();
-//                }
-//            }
+            int prevVolume;
+            int volume;
+            if ("android.media.VOLUME_CHANGED_ACTION".equals(intent.getAction())) {
+                volume = (int) intent.getExtras().get("android.media.EXTRA_VOLUME_STREAM_VALUE");
+                prevVolume = (int) intent.getExtras().get("android.media.EXTRA_PREV_VOLUME_STREAM_VALUE");
+                if (volume < prevVolume) {
+                    Toast.makeText(context, "Volume down!", Toast.LENGTH_SHORT).show();
+
+                } else if (volume > prevVolume) {
+                    Toast.makeText(context, "Volume up!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Volume equals!", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
     }
 }
