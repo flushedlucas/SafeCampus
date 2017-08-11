e3d4c36b00471836f403c6e795291002ad18af89
Lucas Vieira <lucas.filipe.vieira@gmail.com>
2017-08-11 03:51:48 -0300

    ajuste da localização

--- app/src/main/java/com/ufrpe/safecampus/view/TelaInicialActivity.java
+++ app/src/main/java/com/ufrpe/safecampus/view/TelaInicialActivity.java
@@ -42,8 +42,11 @@ public class TelaInicialActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         session.setContext(context);
         createNoGpsDialog();
-        IniciarServico iniciarServico = new IniciarServico(session.getContext());
-        iniciarServico.registrarLocalizacao();
+
+        if (session.getUsuarioLogado().getTipo() !=1) {
+            IniciarServico iniciarServico = new IniciarServico(session.getContext());
+            iniciarServico.registrarLocalizacao();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
